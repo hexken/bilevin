6e0d06a1345e3654d716e7c975106bd0fc4abbd2
Ken Tjhia <KenTjhia@gmail.com>
2022-11-24 14:20:47 -0700

    add run scripts/configs

---
+++ scripts/testrun_stp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python src/main.py \
+    --mode train \
+    --algorithm Levin \
+    --loss levin_loss \
+    --model-path testpath/testmodel.pt \
+    --domain SlidingTile \
+    --problems-path problems/stp/puzzles_3x3_20/ \
+    --initial-budget 7000 \
+    --grad-steps 10 \
+    --batch-size-expansions 32 \
+    --batch-size-bootstrap 5\
