b8ecaea582fea9fa100a5ccfb889351746eef60f
Ken Tjhia <KenTjhia@gmail.com>
2022-12-09 02:24:02 -0700

    fix the relative/abs import error. tested on cc

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 - Gradients probably shouldn't always be scaled by world_size, since not every process contributes
   to the overall gradient (only the ones that solve a problem do).
 - Look into OMP_NUM_TREADS tuning when on ComputeCanada.
+- Fix relative/absolute imports, partircularly environment in slidting_tile_puzzle.py is senseitive
+  to torchrun vs without
 
 # Notes
 - Order of problems seen is only deterministic for a given world_size.
--- scripts/test_levin_stp_torchrun.sh
+++ scripts/test_levin_stp_torchrun.sh
@@ -3,17 +3,17 @@
 torchrun \
     --nnodes=1 \
     --nproc_per_node=4 \
-    --master_addr=127.0.0.1 \
-    --master_port=1234 \
+    --master_addr=$(hostname)\
+    --master_port=34567 \
     src/main.py \
     --mode train \
     --agent Levin \
     --loss cross_entropy_loss \
     --model-path trained_models/ \
     --domain SlidingTile \
-    --problems-path problems/stp_test/3x3_20/ \
+    --problems-path problems/stp_test/3x3_1000/ \
     --initial-budget 7000 \
     --grad-steps 10 \
-    --batch-size-bootstrap 4 \
+    --batch-size-bootstrap 48 \
+    --wandb
     # --cuda
-    # --wandb
