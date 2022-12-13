c8e640ff4f446fe9dee0c7976e0d24703463c523
Ken Tjhia <KenTjhia@gmail.com>
2022-12-13 14:25:06 -0700

    add torch_scatter to requirements. no module purge in cc

--- scripts/cc_train_single.sh
+++ scripts/cc_train_single.sh
@@ -2,13 +2,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=48
 #SBATCH --mem=187G
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 #SBATCH --exclusive
-#SBATCH --output=/scratch/tjhia/bi-levin/slurm_outputs/%j.txt
+#SBATCH --output=/scratch/tjhia/bilevin/slurm_outputs/%j.txt
 #SBATCH --account=def-lelis
 
-module purge
-
 source /home/tjhia/bilevin-env/bin/activate
-cd /scratch/tjhia/bi-levin
-./scripts/test_levin_stp_torchrun.sh
+cd /scratch/tjhia/bilevin
+./scripts/tr3.sh
---
+++ scripts/tr1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+export OMP_NUM_THREADS=1
+
+torchrun \
+    --nnodes=1 \
+    --nproc_per_node=48 \
+    --master_addr=$(hostname)\
+    --master_port=34567 \
+    src/main.py \
+    --mode train \
+    --agent Levin \
+    --loss levin_loss_avg \
+    --model-path trained_models/ \
+    --domain SlidingTile \
+    --problems-path problems/stp/puzzles_5x5_train/ \
+    --initial-budget 7000 \
+    --grad-steps 10 \
+    --batch-size-bootstrap 48 \
+    --wandb
+    # --cuda
