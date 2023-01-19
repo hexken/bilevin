a8b43c749d46b25e9e496338abc98ae0118bd862
Ken Tjhia <KenTjhia@gmail.com>
2023-01-19 14:28:19 -0700

    refactor unused models, reduce ConvNetDouble params

--- src/models/conv_net.py
+++ src/models/conv_net.py
@@ -1,5 +1,4 @@
 import torch as to
-import math
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -60,8 +59,8 @@ class ConvNetDouble(nn.Module):
         self.convblock2 = ConvBlock(
             in_channels, state_t_width, kernel_size, num_filters, num_actions
         )
-        self.linear1 = nn.Linear(num_filters * 2 * self.convblock1.reduced_size, 256)
-        self.linear2 = nn.Linear(256, 128)
+        self.linear1 = nn.Linear(num_filters * 2 * self.convblock1.reduced_size, 128)
+        self.linear2 = nn.Linear(128, 128)
         self.linear3 = nn.Linear(128, num_actions)
 
     def forward(self, x):
@@ -78,51 +77,3 @@ class ConvNetDouble(nn.Module):
         logits = self.linear3(x)
 
         return logits
-
-
-class HeuristicConvNetSingle(nn.Module):
-    def __init__(
-        self, in_channels, state_t_width, kernel_size, num_filters, num_actions
-    ):
-        super().__init__()
-        self.num_actions = num_actions
-        self.convblock = ConvBlock(
-            in_channels, state_t_width, kernel_size, num_filters, num_actions
-        )
-        self.linear1 = nn.Linear(num_filters * self.convblock.reduced_size, 128)
-        self.linear2 = nn.Linear(128, 1)
-
-    def call(self, x):
-        x = self.convblock(x)
-        x = F.relu(self.linear1(x))
-        h = self.linear2(x)
-
-        return h
-
-
-class TwoHeadedConvNetSingle(nn.Module):
-    def __init__(
-        self, in_channels, kernel_size, state_t_width, num_filters, num_actions
-    ):
-        super().__init__()
-        self.num_actions = num_actions
-        self.convblock = ConvBlock(
-            in_channels, state_t_width, kernel_size, num_filters, num_actions
-        )
-
-        self.linear_c1 = nn.Linear(num_filters * self.convblock.reduced_size, 128)
-        self.linear_c2 = nn.Linear(128, num_actions)
-
-        self.linear_h1 = nn.Linear(num_filters * self.convblock.reduced_size, 128)
-        self.linear_h2 = nn.Linear(128, 1)
-
-    def forward(self, x):
-        x = self.convblock(x)
-
-        logits = F.relu(self.linear_c1(x))
-        logits = self.linear_c2(logits)
-
-        h = F.relu(self.linear_h1(x))
-        h = self.linear_h2(h)
-
-        return logits, h
