cf07b32f700d52d7698f565ff5e5e0ffabefa927
Ken Tjhia <KenTjhia@gmail.com>
2023-03-17 22:11:06 -0600

    update run scripts

--- scripts/beluga/cc_train_localenv.sh
+++ scripts/beluga/cc_train_localenv.sh
@@ -2,9 +2,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
 #SBATCH --mem=186G
-#SBATCH --time=3:00:00
+#SBATCH --time=8:00:00
 #SBATCH --exclusive
-#SBATCH --output=/scratch/tjhia/bilevin/slurm_outputs/%j.txt
+#SBATCH --output=/scratch/tjhia/bilevin/slurm_outputs/march17/%j.txt
 #SBATCH --account=rrg-lelis
 
 source $HOME/bilevin-env/bin/activate
@@ -21,15 +21,22 @@ cd /scratch/tjhia/bilevin
 export OMP_NUM_THREADS=1
 
 python src/main.py \
-    --world-size 32 \
+    --world-size 40 \
     --mode train \
-    --agent $1 \
-    --loss levin_loss_sum \
-    --problemset-path $2 \
-    --initial-budget $3 \
+    --loss levin_loss \
     --grad-steps 10 \
-    --batch-size-bootstrap 32 \
-    --seed $4 \
+    --learning-rate 0.001 \
+    --bootstrap-epochs 1 \
+    --curriculum-epochs 1 \
+    --permutation-epochs 10 \
+    --epochs-reduce-lr 1\
+    --epoch-begin-validate 1\
+    --batch-size-train 40 \
+    --agent $1 \
+    --problemset-path $2 \
+    --validset-path $3 \
+    --initial-budget $4 \
+    --seed $5 \
     --wandb-mode offline \
-    --exp-name "_modarch"
+    --exp-name "dset2"\	
 
--- scripts/beluga/runs_train.sh
+++ scripts/beluga/runs_train.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 cd ~/scratch/bilevin
-problemset=problems/witness/7w4c/50000-train.json
-validset=problems/witness/7w4c/1000-valid.json
+problemset=problems/stp/4w-cur/dset2/32000-train.json
+validset=problems/stp/4w-cur/dset2/3200-valid.json
 agents="Levin BiLevin"
 initial_budgets="16000"
-epochs="5"
-seeds="1 43"
+seeds="393 923"
 
 for agent in $agents; do
 	for budget in $initial_budgets; do
-		for epoch in $epochs; do
-			for seed in $seeds; do
-	#			sbatch scripts/beluga/cc_train.sh $agent $problemset $validset $budget $epoch $seed
-				sbatch scripts/beluga/cc_train_localenv.sh $agent $problemset $validset $budget $epoch $seed
-			done
+		for seed in $seeds; do
+#			sbatch scripts/beluga/cc_train.sh $agent $problemset $validset $budget $seed
+			sbatch scripts/beluga/cc_train_localenv.sh $agent $problemset $validset $budget $seed
 		done
 	done
 done
