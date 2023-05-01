aedd1ff8e261e2ca560b043b18764f9b453e3ab8
Ken Tjhia <KenTjhia@gmail.com>
2023-04-30 20:15:41 -0600

    update local run scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -12,10 +12,11 @@ python src/main.py \
     --backward-policy-lr 0.001 \
     --bootstrap-epochs 0 \
     --curriculum-epochs 1 \
-    --permutation-epochs 5 \
-    --permutation-focus \
+    --permutation-epochs 0 \
+    --permutation-focus False \
+    --include-prev-difficulty False \
     --epoch-reduce-lr 99 \
-    --epoch-reduce-grad-steps 2 \
+    --epoch-reduce-grad-steps 99 \
     --epoch-begin-validate 1 \
     --expansion-budget 500 \
     --time-budget 300 \
@@ -23,8 +24,10 @@ python src/main.py \
     --batch-size-train 4 \
     --seed 1 \
     --wandb-mode disabled \
-    --problemset-path problems/stp/april/3w/5000-train.json \
-    --validset-path problems/stp/april/3w/1000-valid.json \
+    --problemset-path problems/sokoban/unfiltered/trainsmall/train.json \
+    --validset-path problems/sokoban/unfiltered/validsmall/valid.json \
+    # --problemset-path problems/stp/april/3w/5000-train.json \
+    # --validset-path problems/stp/april/3w/1000-valid.json \
     # --problemset-path problems/stp/april/5w/50000-train.json \
     # --validset-path problems/stp/april/5w/4000-valid.json \
 
@@ -35,8 +38,6 @@ python src/main.py \
      # --model-path runs/SlidingTilePuzzle-3w-20000-train_BiLevin-1000_1_1681165195 \
     # --problemset-path problems/witness/7w4c/5000-train.json \
     # --validset-path problems/witness/7w4c/500-valid.json \
-    # --problemset-path problems/sokoban/unfiltered/trainsmall/train.json \
-    # --validset-path problems/sokoban/unfiltered/validsmall/valid.json \
     # --include-prev-difficulty \
     # --exp-name "_orig" \
     # --update-levin-costs \
