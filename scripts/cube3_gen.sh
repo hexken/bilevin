57ebea31bf235fd25bcdcbb8a8d2de753d2fdcb7
Ken Tjhia <KenTjhia@gmail.com>
2023-12-06 02:57:12 -0700

    update cube3gen script

--- scripts/cube3_gen.sh
+++ scripts/cube3_gen.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PYTHONPATH=$SCRIPT_DIR/../src
 cd $SCRIPT_DIR/../
 
-if [ $# -ne 1 ]; then
+if [ $# -gt 1 ]; then
     echo "Usage: $0 [d]"
     exit 1
 fi
