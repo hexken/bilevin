9d4f99f6abce8a4061dc28847df5d8cb91895c68
Ken Tjhia <KenTjhia@gmail.com>
2023-12-06 02:48:41 -0700

    update cube3gen script

--- scripts/cube3_gen.sh
+++ scripts/cube3_gen.sh
@@ -3,28 +3,35 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PYTHONPATH=$SCRIPT_DIR/../src
 cd $SCRIPT_DIR/../
 
-python src/domains/puzzle_generator.py \
---domain cube3 \
---seed 3541 \
---output-path  problems/cube3_debug/ \
---n-problems-per-stage  200 \
---randomize-curriculum-steps \
---stages-multiple  5 \
---num-stages  10 \
---final-stage \
---n-valid  100 \
---n-test  100 \
---test-steps 100 \
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 [d]"
+    exit 1
+fi
 
-python src/domains/puzzle_generator.py \
---domain cube3 \
---seed 3541 \
---output-path  problems/cube3/ \
---n-problems-per-stage  100000 \
---randomize-curriculum-steps \
---stages-multiple  5 \
---num-stages  10 \
---final-stage \
---n-valid  1000 \
---n-test  1000 \
---test-steps  100 \
+if [ "$1" = "d" ]; then
+    python src/domains/puzzle_generator.py \
+    --domain cube3 \
+    --seed 3541 \
+    --output-path  problems/cube3_debug/ \
+    --n-problems-per-stage  200 \
+    --randomize-curriculum-steps \
+    --stages-multiple  5 \
+    --num-stages  10 \
+    --final-stage \
+    --n-valid  100 \
+    --n-test  100 \
+    --test-steps 100
+else
+    python src/domains/puzzle_generator.py \
+    --domain cube3 \
+    --seed 3541 \
+    --output-path  problems/cube3/ \
+    --n-problems-per-stage  100000 \
+    --randomize-curriculum-steps \
+    --stages-multiple  5 \
+    --num-stages  10 \
+    --final-stage \
+    --n-valid  1000 \
+    --n-test  1000 \
+    --test-steps  100
+fi
