490e46346f45bf40f5695e94941c20717e802ce3
Ken Tjhia <KenTjhia@gmail.com>
2023-12-06 02:55:09 -0700

    update cube3gen script

--- scripts/beluga/cc_cube3_gen.sh
+++ scripts/beluga/cc_cube3_gen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=rrg-lelis
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
-#SBATCH --mem=24G
-#SBATCH --time=5:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=4G
+#SBATCH --time=1:30:00
 #SBATCH --output=/scratch/tjhia/bilevin/slurm_outputs/%j.out
 
 source $HOME/bilevin-env2/bin/activate
