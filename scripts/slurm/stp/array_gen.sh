bfb5a539950d10ca70dcf0ae63c425f3554e8bb4
Ken Tjhia <KenTjhia@gmail.com>
2024-01-29 16:38:19 -0700

    add weight-mse-loss arg

--- scripts/slurm/stp/array_gen.sh
+++ scripts/slurm/stp/array_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-lrs="0.0001 0.0005 0.001 0.005, 0.01"
+lrs="0.0001 0.0005 0.001 0.005 0.01"
 losses="traj_nll_mse_loss levin_avg_mse_loss levin_sum_mse_loss"
 seeds="1 2 3"
 max_grad_norms="-1.0 0.1 1.0"
