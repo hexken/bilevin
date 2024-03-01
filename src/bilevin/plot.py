303583590d81370f2074db8071f9f3d6376306f6
Ken Tjhia <KenTjhia@gmail.com>
2024-03-01 09:01:30 -0700

    update plotting code

--- src/bilevin/plot.py
+++ src/bilevin/plot.py
@@ -109,7 +109,7 @@ def plot_search_vs_batch(
     label=None,
     batch_size=4,
     n_final_stage_epochs=25,
-    window_size=75,
+    window_size=100,
 ):
     # plot seeds corresponding to a run
     dfs = run_data["search"]
@@ -178,10 +178,12 @@ def plot_search_vs_time(
     )
 
 
-def window_avgs(data: list[pd.DataFrame], y_data_label, window_size=75):
+def window_avgs(data: list[pd.DataFrame], y_data_label, window_size=100):
     aggr_dfs = []
+    max_batch = max(df.index.values[-1] for df in data)
     for df in data:
         df = df[y_data_label]
+        df = df.reindex(range(1, max_batch + 1), fill_value=np.nan)
         df = df.rolling(window_size, min_periods=1).mean()
         aggr_dfs.append(df)
 
@@ -209,9 +211,16 @@ def plot_medians(ax, run_data, y_label, color):
 def plot_bi_policy_model(run_data: dict, label: str):
     fig, ax = plt.subplots(2, 2, figsize=(12, 10), sharex=True)
     fl = ax[0, 0]
+    fl.set_title(f"Forward")
+    fl.set_ylabel("Loss")
     fa = ax[1, 0]
+    fa.set_ylabel("Acc")
+    fa.set_xlabel("Batch")
+
     bl = ax[0, 1]
+    bl.set_title(f"Backward")
     ba = ax[1, 1]
+    ba.set_xlabel("Batch")
 
     ls_mapper = LineStyleMapper()
     color, _, _ = ls_mapper.get_ls(label)
@@ -220,70 +229,108 @@ def plot_bi_policy_model(run_data: dict, label: str):
     plot_medians(fa, run_data["train"], "facc", color)
     plot_medians(bl, run_data["train"], "bloss", color)
     plot_medians(ba, run_data["train"], "bacc", color)
-    return fig
-
-
-def plot_bi_heuristic_model(run_data: dict, label: str):
-    fig, ax = plt.subplots(1, 2, figsize=(12, 10), sharex=True)
-    fl = ax[0, 0]
-    bl = ax[0, 1]
-
-    ls_mapper = LineStyleMapper()
-    color, _, _ = ls_mapper.get_ls(label)
-
-    plot_medians(fl, run_data["train"], "floss", color)
-    plot_medians(bl, run_data["train"], "bloss", color)
-    return fig
+    return fig, ax
 
 
 def plot_uni_policy_model(run_data: dict, label: str):
     fig, ax = plt.subplots(2, 1, figsize=(12, 10), sharex=True)
     fl = ax[0]
+    fl.set_title(f"Forward")
+    fl.set_ylabel("Loss")
     fa = ax[1]
+    fa.set_xlabel("Batch")
 
     ls_mapper = LineStyleMapper()
     color, _, _ = ls_mapper.get_ls(label)
 
     plot_medians(fl, run_data["train"], "floss", color)
     plot_medians(fa, run_data["train"], "facc", color)
-    return fig
+    return fig, ax
+
+
+def plot_bi_heuristic_model(run_data: dict, label: str):
+    fig, ax = plt.subplots(1, 2, figsize=(12, 10), sharex=True)
+    fl = ax[0]
+    fl.set_title(f"Forward")
+    fl.set_ylabel("Loss")
+    fl.set_xlabel("Batch")
+    bl = ax[1]
+    bl.set_title(f"Backward")
+    bl.set_xlabel("Batch")
+
+    ls_mapper = LineStyleMapper()
+    color, _, _ = ls_mapper.get_ls(label)
+
+    plot_medians(fl, run_data["train"], "floss", color)
+    plot_medians(bl, run_data["train"], "bloss", color)
+    return fig, ax
 
 
 def plot_uni_heuristic_model(run_data: dict, label: str):
     fig, ax = plt.subplots(1, 1, figsize=(12, 10), sharex=True)
     fl = ax
+    fl.set_title(f"Forward")
+    fl.set_ylabel("Loss")
+    fl.set_xlabel("Batch")
     ls_mapper = LineStyleMapper()
     color, _, _ = ls_mapper.get_ls(label)
 
     plot_medians(fl, run_data["train"], "floss", color)
-    return fig
+    return fig, ax
 
 
 def plot_bi_policy_heuristic_model(run_data: dict, label: str):
-    fig, ax = plt.subplots(3, 2, figsize=(12, 10), sharex=True)
-    flp = ax[0, 0]
+    fig, ax = plt.subplots(2, 2, figsize=(12, 10), sharex=True)
+    fl = ax[0, 0]
+    fl.set_title(f"Forward")
+    fl.set_ylabel("Loss")
     fa = ax[1, 0]
-    flh = ax[2, 1]
+    fa.set_ylabel("Acc")
+    fa.set_xlabel("Batch")
+    # flh = ax[2, 1]
 
-    blp = ax[0, 1]
+    bl = ax[0, 1]
+    bl.set_title(f"Backward")
     ba = ax[1, 1]
-    blh = ax[2, 1]
+    ba.set_xlabel("Batch")
+    # blh = ax[2, 1]
+
+    ls_mapper = LineStyleMapper()
+    color, _, _ = ls_mapper.get_ls(label)
+
+    # flp.set_ylabel("Policy loss", size=14)
+    plot_medians(fl, run_data["train"], "floss", color)
+    plot_medians(fa, run_data["train"], "facc", color)
+    # plot_medians(flh, run_data["train"], "bloss", color)
+
+    plot_medians(bl, run_data["train"], "bloss", color)
+    plot_medians(ba, run_data["train"], "bacc", color)
+    # plot_medians(blh, run_data["train"], "bacc", color)
+    return fig, ax
+
+
+def plot_uni_policy_heuristic_model(run_data: dict, label: str):
+    fig, ax = plt.subplots(2, 1, figsize=(12, 10), sharex=True)
+    flp = ax[0]
+    flp.set_title(f"Forward")
+    flp.set_ylabel("Loss")
+    fa = ax[1]
+    fa.set_ylabel("Acc")
+    fa.set_xlabel("Batch")
+    # flh = ax[2]
 
     ls_mapper = LineStyleMapper()
     color, _, _ = ls_mapper.get_ls(label)
 
     plot_medians(flp, run_data["train"], "floss", color)
     plot_medians(fa, run_data["train"], "facc", color)
-    plot_medians(flh, run_data["train"], "bloss", color)
+    # plot_medians(flh, run_data["train"], "bloss", color)
 
-    plot_medians(blp, run_data["train"], "bloss", color)
-    plot_medians(ba, run_data["train"], "bacc", color)
-    plot_medians(blh, run_data["train"], "bacc", color)
-    return fig
+    return fig, ax
 
 
-def plot_domain(run_data: dict, outdir_root: str):
-    saveroot = Path(outdir_root)
+def plot_domain(run_data: dict, outdir: str):
+    saveroot = Path(outdir)
     saveroot.mkdir(exist_ok=True, parents=True)
     figkey = ["problems_path"]
     legendkey = ["agent"]
@@ -296,8 +343,8 @@ def plot_domain(run_data: dict, outdir_root: str):
         group = sorted(group, key=lambda x: x.args_key(legendkey))
         grouped_data = {}
         for runseeds in group:
-            legend_name = runseeds.args_key(legendkey)
-            grouped_data[legend_name] = runseeds
+            agent = runseeds.args_key(legendkey)
+            grouped_data[agent] = runseeds
 
         # search and val
         fig, ax = plt.subplots(3, 2, figsize=(12, 10))
@@ -317,60 +364,77 @@ def plot_domain(run_data: dict, outdir_root: str):
         ax2[2].set_ylabel("Solution length", size=14)
 
         labels = []
-        for i, (legend_name, runseeds) in enumerate(grouped_data.items()):
-            style = ls_mapper.get_ls(legend_name)
-            # c = colors[i % len(colors)]
-
-            # search and val
+        # loop over agents (uni/bi - levin/phs/astar, etc.)
+        for agent, runseeds in grouped_data.items():
+            style = ls_mapper.get_ls(agent)
+            # fill search and val plots
             rsdata = runseeds.data
-            labels.append(legend_name)
+            labels.append(agent)
             plot_search(
                 rsdata,
                 "solved",
                 ax=ax[0, 0],
                 style=style,
-                label=legend_name,
+                label=agent,
             )
-            plot_search(rsdata, "exp", ax=ax[1, 0], style=style, label=legend_name)
-            plot_search(rsdata, "len", ax=ax[2, 0], style=style, label=legend_name)
-            # ax[2, 0].set_xlabel("Time (s)", size=14)
+            plot_search(rsdata, "exp", ax=ax[1, 0], style=style, label=agent)
+            plot_search(rsdata, "len", ax=ax[2, 0], style=style, label=agent)
 
             plot_valid(
                 rsdata,
                 "solved",
                 ax=ax[0, 1],
                 style=style,
-                label=legend_name,
+                label=agent,
             )
-            plot_valid(rsdata, "exp", ax=ax[1, 1], style=style, label=legend_name)
-            plot_valid(rsdata, "len", ax=ax[2, 1], style=style, label=legend_name)
-            # ax[2, 1].set_xlabel("Time (s)", size=14)
+            plot_valid(rsdata, "exp", ax=ax[1, 1], style=style, label=agent)
+            plot_valid(rsdata, "len", ax=ax[2, 1], style=style, label=agent)
+            ax[2, 1].set_xlabel("Valid #", size=14)
 
-            # search vs batch
+            # fill search vs batch plots
             plot_search_vs_batch(
                 rsdata,
                 "solved",
                 ax=ax2[0],
                 style=style,
-                label=legend_name,
+                label=agent,
             )
-            plot_search_vs_batch(
-                rsdata, "exp", ax=ax2[1], style=style, label=legend_name
-            )
-            plot_search_vs_batch(
-                rsdata, "len", ax=ax2[2], style=style, label=legend_name
-            )
-            print(f"Plotted {fkey} {legend_name}")
+            plot_search_vs_batch(rsdata, "exp", ax=ax2[1], style=style, label=agent)
+            plot_search_vs_batch(rsdata, "len", ax=ax2[2], style=style, label=agent)
+            # make model train plots
+            if "Bi" in agent:
+                if "PHS" in agent:
+                    f, a = plot_bi_policy_heuristic_model(rsdata, agent)
+                elif "Levin" in agent:
+                    f, a = plot_bi_policy_model(rsdata, agent)
+                elif "AStar" in agent:
+                    f, a = plot_bi_heuristic_model(rsdata, agent)
+                else:
+                    raise ValueError(f"Unknown agent: {agent}")
+            else:
+                if "PHS" in agent:
+                    f, a = plot_uni_policy_heuristic_model(rsdata, agent)
+                elif "Levin" in agent:
+                    f, a = plot_uni_policy_model(rsdata, agent)
+                elif "AStar" in agent:
+                    f, a = plot_uni_heuristic_model(rsdata, agent)
+                else:
+                    raise ValueError(f"Unknown agent: {agent}")
+            f.savefig(saveroot / f"{agent}_model.pdf", bbox_inches="tight")
+            plt.close()
+            print(f"Plotted {fkey} {agent}")
 
         handles, labels = ax[2, 1].get_legend_handles_labels()
         it = iter(handles)
         handles = [(a, next(it)) for a in it]
         fig.legend(handles, labels[::2])
         fig.tight_layout()
-        fig.savefig(saveroot / f"{fkey}.pdf", bbox_inches="tight")
+        fig.savefig(saveroot / f"search_valid.pdf", bbox_inches="tight")
+        plt.close()
 
         fig2.tight_layout()
-        fig2.savefig(saveroot / f"{fkey}_batch.pdf", bbox_inches="tight")
+        fig2.savefig(saveroot / f"search_vs_batch.pdf", bbox_inches="tight")
+        plt.close()
 
 
 def main():
@@ -380,7 +444,7 @@ def main():
     for dom in dom_paths:
         print(dom.stem)
 
-    for dom in dom_paths[-1:]:
+    for dom in dom_paths:
         dom_data = pkl.load(dom.open("rb"))
         plot_domain(dom_data, f"figs/thes/{dom.stem}")
 
