c5d219fe7cc7bec05d26cd1c02ba7987dda333ee
Ken Tjhia <KenTjhia@gmail.com>
2024-03-08 16:37:37 -0700

    update prepare_test.py process all domains

--- scripts/prepare_test.py
+++ scripts/prepare_test.py
@@ -3,19 +3,8 @@ import sys
 
 from natsort import natsorted
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python prepare_test.py <domain_dir> <best|last>")
-        sys.exit(1)
 
-    if sys.argv[2] == "best":
-        model_suffix = "_best_expanded.pt"
-    elif sys.argv[2] == "last":
-        model_suffix = "_lastest.pt"
-    else:
-        raise ValueError("Invalid model suffix")
-
-    domdir = Path(sys.argv[1])
+def prepare_domain(domdir, model_suffix):
     for agentdir in domdir.glob("*/"):
         if agentdir.name == "astar":
             outfile = domdir / "astar_args_test.txt"
@@ -37,8 +26,31 @@ if __name__ == "__main__":
             elif agents[1] in rundir.name:
                 i = 1
             else:
+                print(f"Skipping {rundir.name}")
                 continue
-            args = f"{agents[i][1:]} runs/thes/{model_pth}"
+            args = f"{agents[i][1:]} {model_pth}"
             runargs.append(args)
 
         outfile.write_text("\n".join(runargs))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python prepare_test.py <dom_dir|doms_dir> [all] <best|last>")
+        sys.exit(1)
+
+    d = sys.argv[1]
+    if sys.argv[2].lower() == "all":
+        dom_dirs = list(Path(d).glob("*/"))
+    else:
+        dom_dirs = [Path(d)]
+
+    if sys.argv[3] == "best":
+        model_suffix = "_best_expanded.pt"
+    elif sys.argv[4] == "last":
+        model_suffix = "_lastest.pt"
+    else:
+        raise ValueError("Invalid model suffix")
+
+    for domdir in dom_dirs:
+        prepare_domain(domdir, model_suffix)
