7499c56833c8e3f78e9839f6b0d59818ffc579c8
Ken Tjhia <KenTjhia@gmail.com>
2024-03-20 20:34:24 -0600

    add train_all script

--- scripts/slurm/train_all.sh
+++ scripts/slurm/train_all.sh
@@ -6,3 +6,32 @@ pancakes_dir=$scripts_dir/pancake
 tri_dir=$scripts_dir/tri
 col_dir=$scripts_dir/col
 
+agents="levin astar phs"
+pancake_sizes="14 18 22"
+stp_sizes="4 5"
+tri_sizes="4 5"
+col_sizes="4 5"
+
+for agent in $agents; do
+    for size in $pancake_sizes; do
+        sbatch $pancakes_dir/pancake${size}_${agent}.sh
+    done
+done
+
+for agent in $agents; do
+    for size in $stp_sizes; do
+        sbatch $stp_dir/stp${size}_${agent}.sh
+    done
+done
+
+for agent in $agents; do
+    for size in $tri_sizes; do
+        sbatch $tri_dir/tri${size}_${agent}.sh
+    done
+done
+
+for agent in $agents; do
+    for size in $col_sizes; do
+        sbatch $col_dir/col${size}_${agent}.sh
+    done
+done
