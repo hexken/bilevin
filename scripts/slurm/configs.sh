6d08f301b77de3ec5175c7e5f6ec0b59259d0963
Ken Tjhia <KenTjhia@gmail.com>
2024-07-10 18:37:26 -0600

    update configs.sh

--- scripts/slurm/configs.sh
+++ scripts/slurm/configs.sh
@@ -1,22 +1,42 @@
-# stp5 debug
-time=00:10:00 array=1 dom=stp5 agent=PHS sfn=True budget=7000 filename=stp5_phs_debug.sh
-time=00:10:00 array=1 dom=stp5 agent=BiPHS sfn=True budget=7000 filename=stp5_biphs_debug.sh
-time=00:10:00 array=1 dom=stp5 agent=Levin sfn=True budget=7000 filename=stp5_levin_debug.sh
-time=00:10:00 array=1 dom=stp5 agent=BiLevin sfn=True budget=7000 filename=stp5_bilevin.sh
-time=00:10:00 array=1 dom=stp5 agent=AStar sfn=True budget=7000 filename=stp5_astar_debug.sh
-time=00:10:00 array=1 dom=stp5 agent=BiAStar sfn=True budget=7000 filename=stp5_biastar_debug.sh
+# 24 hrs PHS, 
+time=00:10:00 array=1 dom=stp4 agent=PHS sfn=False budget=7000 filename=stp4_phs.sh
+time=00:10:00 array=1 dom=stp4 agent=BiPHS sfn=False budget=7000 filename=stp4_biphs.sh
+time=00:10:00 array=1 dom=stp4 agent=Levin sfn=False budget=7000 filename=stp4_levin.sh
+time=00:10:00 array=1 dom=stp4 agent=BiLevin sfn=False budget=7000 filename=stp4_bilevin.sh
+time=00:10:00 array=1 dom=stp4 agent=AStar sfn=False budget=7000 filename=stp4_astar.sh
+time=00:10:00 array=1 dom=stp4 agent=BiAStar sfn=False budget=7000 filename=stp4_biastar.sh
 
-# stp4 debug
-time=00:10:00 array=1 dom=stp4 agent=PHS sfn=True budget=2000 filename=stp4_phs_debug.sh
-time=00:10:00 array=1 dom=stp4 agent=BiPHS sfn=True budget=2000 filename=stp4_biphs_debug.sh
-time=00:10:00 array=1 dom=stp4 agent=Levin sfn=True budget=2000 filename=stp4_levin_debug.sh
-time=00:10:00 array=1 dom=stp4 agent=BiLevin sfn=True budget=2000 filename=stp4_bilevin.sh
-time=00:10:00 array=1 dom=stp4 agent=AStar sfn=True budget=2000 filename=stp4_astar_debug.sh
-time=00:10:00 array=1 dom=stp4 agent=BiAStar sfn=True budget=2000 filename=stp4_biastar_debug.sh
+time=00:10:00 array=1 dom=stp5 agent=PHS sfn=False budget=7000 filename=stp5_phs.sh
+time=00:10:00 array=1 dom=stp5 agent=BiPHS sfn=False budget=7000 filename=stp5_biphs.sh
+time=00:10:00 array=1 dom=stp5 agent=Levin sfn=False budget=7000 filename=stp5_levin.sh
+time=00:10:00 array=1 dom=stp5 agent=BiLevin sfn=False budget=7000 filename=stp5_bilevin.sh
+time=00:10:00 array=1 dom=stp5 agent=AStar sfn=False budget=7000 filename=stp5_astar.sh
+time=00:10:00 array=1 dom=stp5 agent=BiAStar sfn=False budget=7000 filename=stp5_biastar.sh
 
-# time=00:10:00 array=1 dom=stp5 agent=PHS sfn=True budget=7000 filename=stp5_phs.sh
-# time=00:10:00 array=1 dom=stp5 agent=BiPHS sfn=True budget=7000 filename=stp5_phs.sh
-# time=00:10:00 array=1 dom=stp5 agent=Levin sfn=True budget=7000 filename=stp5_phs.sh
-# time=00:10:00 array=1 dom=stp5 agent=BiLevin sfn=True budget=7000 filename=stp5_phs.sh
-# time=00:10:00 array=1 dom=stp5 agent=AStar sfn=True budget=7000 filename=stp5_phs.sh
-# time=00:10:00 array=1 dom=stp5 agent=BiAStar sfn=True budget=7000 filename=stp5_phs.sh
+time=00:10:00 array=1 dom=tri4 agent=PHS sfn=False budget=2000 filename=tri4_phs.sh
+time=00:10:00 array=1 dom=tri4 agent=BiPHS sfn=False budget=2000 filename=tri4_biphs.sh
+time=00:10:00 array=1 dom=tri4 agent=Levin sfn=False budget=2000 filename=tri4_levin.sh
+time=00:10:00 array=1 dom=tri4 agent=BiLevin sfn=False budget=2000 filename=tri4_bilevin.sh
+time=00:10:00 array=1 dom=tri4 agent=AStar sfn=False budget=2000 filename=tri4_astar.sh
+time=00:10:00 array=1 dom=tri4 agent=BiAStar sfn=False budget=2000 filename=tri4_biastar.sh
+
+time=00:10:00 array=1 dom=tri5 agent=PHS sfn=False budget=7000 filename=tri5_phs.sh
+time=00:10:00 array=1 dom=tri5 agent=BiPHS sfn=False budget=7000 filename=tri5_biphs.sh
+time=00:10:00 array=1 dom=tri5 agent=Levin sfn=False budget=7000 filename=tri5_levin.sh
+time=00:10:00 array=1 dom=tri5 agent=BiLevin sfn=False budget=7000 filename=tri5_bilevin.sh
+time=00:10:00 array=1 dom=tri5 agent=AStar sfn=False budget=7000 filename=tri5_astar.sh
+time=00:10:00 array=1 dom=tri5 agent=BiAStar sfn=False budget=7000 filename=tri5_biastar.sh
+
+time=00:10:00 array=1 dom=col4 agent=PHS sfn=False budget=2000 filename=col4_phs.sh
+time=00:10:00 array=1 dom=col4 agent=BiPHS sfn=False budget=2000 filename=col4_biphs.sh
+time=00:10:00 array=1 dom=col4 agent=Levin sfn=False budget=2000 filename=col4_levin.sh
+time=00:10:00 array=1 dom=col4 agent=BiLevin sfn=False budget=2000 filename=col4_bilevin.sh
+time=00:10:00 array=1 dom=col4 agent=AStar sfn=False budget=2000 filename=col4_astar.sh
+time=00:10:00 array=1 dom=col4 agent=BiAStar sfn=False budget=2000 filename=col4_biastar.sh
+
+time=00:10:00 array=1 dom=col5 agent=PHS sfn=False budget=7000 filename=col5_phs.sh
+time=00:10:00 array=1 dom=col5 agent=BiPHS sfn=False budget=7000 filename=col5_biphs.sh
+time=00:10:00 array=1 dom=col5 agent=Levin sfn=False budget=7000 filename=col5_levin.sh
+time=00:10:00 array=1 dom=col5 agent=BiLevin sfn=False budget=7000 filename=col5_bilevin.sh
+time=00:10:00 array=1 dom=col5 agent=AStar sfn=False budget=7000 filename=col5_astar.sh
+time=00:10:00 array=1 dom=col5 agent=BiAStar sfn=False budget=7000 filename=col5_biastar.sh
