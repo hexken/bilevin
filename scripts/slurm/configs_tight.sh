82a2e6737696a351dd1e3347fdb6ab15d8b3d792
Ken Tjhia <KenTjhia@gmail.com>
2024-07-13 17:50:12 -0600

    update configs

---
+++ scripts/slurm/configs_tight.sh
@@ -0,0 +1,41 @@
+time=12:00:00 mem=4G dom=stp4 agent=PHS sfn=False budget=4000 fname=stp4_phs.sh
+time=12:00:00 mem=4G dom=stp4 agent=Levin sfn=False budget=4000 fname=stp4_levin.sh
+time=12:00:00 mem=4G dom=stp4 agent=AStar sfn=False budget=4000 fname=stp4_astar.sh
+time=28:00:00 mem=4G dom=stp4 agent=BiPHS sfn=False budget=4000 fname=stp4_biphs.sh
+time=18:00:00 mem=4G dom=stp4 agent=BiLevin sfn=False budget=4000 fname=stp4_bilevin.sh
+time=16:00:00 mem=4G dom=stp4 agent=BiAStar sfn=False budget=4000 fname=stp4_biastar.sh
+
+time=28:00:00 mem=5G dom=stp5 agent=PHS sfn=False budget=7000 fname=stp5_phs.sh
+time=28:00:00 mem=5G dom=stp5 agent=Levin sfn=False budget=7000 fname=stp5_levin.sh
+time=24:00:00 mem=5G dom=stp5 agent=AStar sfn=False budget=7000 fname=stp5_astar.sh
+time=52:00:00 mem=8G dom=stp5 agent=BiPHS sfn=False budget=7000 fname=stp5_biphs.sh #
+time=52:00:00 mem=8G dom=stp5 agent=BiLevin sfn=False budget=7000 fname=stp5_bilevin.sh #
+time=26:00:00 mem=6G dom=stp5 agent=BiAStar sfn=False budget=7000 fname=stp5_biastar.sh
+
+time=10:00:00 mem=6G dom=tri4 agent=PHS sfn=False budget=2000 fname=tri4_phs.sh
+time=10:00:00 mem=6G dom=tri4 agent=Levin sfn=False budget=2000 fname=tri4_levin.sh
+time=10:00:00 mem=6G dom=tri4 agent=AStar sfn=False budget=2000 fname=tri4_astar.sh
+time=16:00:00 mem=6G dom=tri4 agent=BiPHS sfn=False budget=2000 fname=tri4_biphs.sh
+time=12:00:00 mem=6G dom=tri4 agent=BiLevin sfn=False budget=2000 fname=tri4_bilevin.sh
+time=14:00:00 mem=6G dom=tri4 agent=BiAStar sfn=False budget=2000 fname=tri4_biastar.sh
+
+time=12:00:00 mem=7G dom=tri5 agent=PHS sfn=False budget=4000 fname=tri5_phs.sh
+time=12:00:00 mem=7G dom=tri5 agent=Levin sfn=False budget=4000 fname=tri5_levin.sh
+time=20:00:00 mem=7G dom=tri5 agent=AStar sfn=False budget=4000 fname=tri5_astar.sh
+time=16:00:00 mem=7G dom=tri5 agent=BiPHS sfn=False budget=4000 fname=tri5_biphs.sh
+time=56:00:00 mem=7G dom=tri5 agent=BiLevin sfn=False budget=4000 fname=tri5_bilevin.sh
+time=22:00:00 mem=7G dom=tri5 agent=BiAStar sfn=False budget=4000 fname=tri5_biastar.sh
+
+time=10:00:00 mem=6G dom=col4 agent=PHS sfn=False budget=2000 fname=col4_phs.sh
+time=10:00:00 mem=6G dom=col4 agent=Levin sfn=False budget=2000 fname=col4_levin.sh
+time=14:00:00 mem=6G dom=col4 agent=AStar sfn=False budget=2000 fname=col4_astar.sh
+time=28:00:00 mem=6G dom=col4 agent=BiPHS sfn=False budget=2000 fname=col4_biphs.sh
+time=28:00:00 mem=6G dom=col4 agent=BiLevin sfn=False budget=2000 fname=col4_bilevin.sh
+time=28:00:00 mem=6G dom=col4 agent=BiAStar sfn=False budget=2000 fname=col4_biastar.sh
+
+time=42:00:00 mem=9G dom=col5 agent=PHS sfn=False budget=4000 fname=col5_phs.sh
+time=42:00:00 mem=9G dom=col5 agent=Levin sfn=False budget=4000 fname=col5_levin.sh
+time=42:00:00 mem=9G dom=col5 agent=AStar sfn=False budget=4000 fname=col5_astar.sh
+time=56:00:00 mem=9G dom=col5 agent=BiPHS sfn=False budget=4000 fname=col5_biphs.sh
+time=56:00:00 mem=9G dom=col5 agent=BiLevin sfn=False budget=4000 fname=col5_bilevin.sh
+time=56:00:00 mem=9G dom=col5 agent=BiAStar sfn=False budget=4000 fname=col5_biastar.sh
