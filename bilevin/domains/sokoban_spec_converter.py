a130d67845222021e3e90e8d3740fffee5de6edc
Ken Tjhia <KenTjhia@gmail.com>
2024-09-17 10:54:26 -0400

    update sok spec conv, heuristically find backward start states

--- bilevin/domains/sokoban_spec_converter.py
+++ bilevin/domains/sokoban_spec_converter.py
@@ -26,6 +26,34 @@ def empty_neighbours(r, c, map):
             yield (rr, cc)
 
 
+def goal_adj_connected_components(map):
+    goal_adj = set()
+    for r, c in zip(*np.where(map[Sokoban.box_goal_channel])):
+        for pos in empty_neighbours(r, c, map):
+            goal_adj.add(pos)
+
+    # reachables = []
+    components = []
+    visited = set()
+    for adj in goal_adj:
+        if adj in visited:
+            continue
+        component = [adj]
+        visited.add(adj)
+        frontier = deque([adj])
+        while len(frontier) > 0:
+            pos = frontier.popleft()
+            r, c = pos
+            for new_pos in empty_neighbours(r, c, map):
+                if new_pos not in visited:
+                    frontier.append(new_pos)
+                    visited.add(new_pos)
+                    component.append(new_pos)
+        # heappush(reachables, (-count, adj))
+        components.append(component)
+    return components
+
+
 def count_reachable_positions(map):
     candidates = set()
     for r, c in zip(*np.where(map[Sokoban.box_goal_channel])):
@@ -84,23 +112,25 @@ def main():
     for f in tqdm.tqdm(problem_files):
         all_txt = f.read_text()
         all_problem_strings = all_txt.split(";")
-        for problem_string in all_problem_strings[:5]:
+        for problem_string in all_problem_strings:
             if len(problem_string) < 20:
                 continue
             lines = problem_string.splitlines()
-            lines = lines[1:-1]
+            if lines[-1] == "":
+                lines = lines[:-1]
+            lines = lines[1:]
+            assert len(lines) == 10
             print("orignial string")
             print("\n".join(lines))
 
-            rows = len(lines)
-            cols = len(lines[0])
-            map = np.zeros((3, rows, cols), dtype=np.float32)
-            boxes = np.zeros((rows, cols), dtype=np.float32)
+            width = len(lines)
+            map = np.zeros((3, width, width), dtype=np.float32)
+            boxes = np.zeros((width, width), dtype=np.float32)
             man_row = -1
             man_col = -1
 
-            for i in range(rows):
-                for j in range(cols):
+            for i in range(width):
+                for j in range(width):
                     if lines[i][j] == Sokoban.goal_str:
                         map[Sokoban.box_goal_channel, i, j] = 1
 
@@ -113,22 +143,31 @@ def main():
 
                     if lines[i][j] == Sokoban.box_str:
                         boxes[i, j] = 1
-            assert 1 <= man_row <= rows - 2
-            assert 1 <= man_col <= cols - 2
+            assert 1 <= man_row <= width - 2
+            assert 1 <= man_col <= width - 2
             assert (boxes == 1).sum() == 4
 
             state = SokobanState(man_row, man_col, boxes)
             # todo set man_goal_channel
-            reachables = count_reachable_positions(map)
-            r, c = reachables[0][1]
-            print(reachables)
-            map[Sokoban.man_goal_channel, r, c] = 1
+            components = goal_adj_connected_components(map)
+            # print(components)
+            rows, cols = list(zip(*[c[0] for c in components]))
+            map[Sokoban.man_goal_channel, rows, cols] = 1
 
             domain = Sokoban(state, map, forward=True)
-            print("converted string")
-            domain.print(state)
-            ts = domain.state_tensor(domain.init())
-            # print(ts)
+            # print to debug
+            if len(components) > 1:
+                print(len(components))
+                print(components)
+                f_state = domain.init()
+                domain.print(f_state)
+                print()
+                b_domain = domain.backward_domain()
+                b_states = b_domain.init()
+                for b_state in b_states:
+                    b_domain.print(b_state)
+                    print()
+                exit()
             problem = Problem(id=id_counter, domain=domain)
             problems.append(problem)
             id_counter += 1
